f51803130f8f591ba6d0ccf628dad31802e87153
Alexey Snytnikov <aleksej.snytnikov@klgtu.ru>
2025-01-16 20:55:06 +0200

    added debug output to study dataset

--- GravNN/Networks/Data.py
+++ GravNN/Networks/Data.py
@@ -674,6 +674,11 @@ class DataSet:
                 exit("No dtype specified")
 
         dataset = tf.data.Dataset.from_tensor_slices((x, y))
+        for i, elem in enumerate(dataset):
+            x = elem[0].numpy()
+            print(i, elem[0].numpy())
+            print(i,x[-1])
+            
         if shuffle:
             dataset = dataset.shuffle(len(x), seed=1234)
         dataset = dataset.batch(
